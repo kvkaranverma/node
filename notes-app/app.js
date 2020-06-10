43128f420ebda2286350a4d747c46544af27c1a6
kvkaranverma <42038789+kvkaranverma@users.noreply.github.com>
2020-06-10 06:28:17 +0530

    importing my own files in app.js

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,17 @@
 const fs = require('fs')
+const utils = require('./utils.js')
+const notes = require('./notes')
 
 fs.writeFileSync('notes.txt', 'This file was created by node js!')
 
 fs.appendFileSync('notes.txt', ' Karan here!')
+
+const name = 'Karan'
+
+console.log(name)
+
+console.log(utils.name_2)
+
+console.log(utils.add(2, 3))
+
+console.log(notes())
---
+++ notes-app/utils.js
@@ -0,0 +1,11 @@
+
+
+console.log('utils.js')
+
+const name_2 = 'Mike'
+
+const add = function(a, b){
+    return a + b
+}
+
+module.exports = {name_2, add}
