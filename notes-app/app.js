3c2bba63bff090484314c3b34b3648c71e8bb2bd
kvkaranverma <42038789+kvkaranverma@users.noreply.github.com>
2020-06-10 07:01:49 +0530

    importing npm modules/packages

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,17 +1,8 @@
-const fs = require('fs')
-const utils = require('./utils.js')
 const notes = require('./notes')
-
-fs.writeFileSync('notes.txt', 'This file was created by node js!')
-
-fs.appendFileSync('notes.txt', ' Karan here!')
-
-const name = 'Karan'
-
-console.log(name)
-
-console.log(utils.name_2)
-
-console.log(utils.add(2, 3))
+const validator = require('validator')
 
 console.log(notes())
+
+console.log(validator.isEmail('karan@gmail.com'))
+
+console.log(validator.isURL('http:/www.google.com'))
