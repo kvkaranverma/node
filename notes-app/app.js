1e6438f5e38804ab0307ce390db25e6506099d32
kvkaranverma <42038789+kvkaranverma@users.noreply.github.com>
2020-06-10 06:59:34 +0530

    Revert "importing npm packages"
    This reverts commit 4d04b5de252ff7c31cd638d746b02d7bc68cdd41.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,17 @@
+const fs = require('fs')
+const utils = require('./utils.js')
 const notes = require('./notes')
-const validator = require('validator')
+
+fs.writeFileSync('notes.txt', 'This file was created by node js!')
+
+fs.appendFileSync('notes.txt', ' Karan here!')
+
+const name = 'Karan'
+
+console.log(name)
+
+console.log(utils.name_2)
+
+console.log(utils.add(2, 3))
 
 console.log(notes())
-
-console.log(validator.isEmail('karan@gmail.com'))
-
-console.log(validator.isURL('http:/www.google.com'))
