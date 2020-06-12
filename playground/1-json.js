3adb4ff25ebb257db0065d16fe6dc0ab855f9d71
kvkaranverma <42038789+kvkaranverma@users.noreply.github.com>
2020-06-12 19:09:39 +0530

    Reading json data from a json file

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,9 +1,14 @@
 const fs = require('fs')
 
-const Book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday'
-}
+// const Book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday'
+// }
 
-const bookJSON = JSON.stringify(Book)
-fs.writeFileSync('1-json.json', bookJSON)
+// const bookJSON = JSON.stringify(Book)
+//fs.writeFileSync('1-json.json', bookJSON)
+
+const dataBuffer = fs.readFileSync('1-json.json')
+const dataJSON = dataBuffer.toString()
+const data = JSON.parse(dataJSON)
+console.log(data.title)
