15870aa40e1893870182f27d43b3967bdec8aa7e
kvkaranverma <42038789+kvkaranverma@users.noreply.github.com>
2020-06-12 18:31:44 +0530

    Creating custom commands using yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,7 +2,40 @@ const notes = require('./notes')
 const chalk = require('chalk')
 const yargs = require('yargs')
 
-console.log(process.argv)
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function(){
+        console.log('Adding a new note!')
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Remove the note')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'Listing notes',
+    handler: function(){
+        console.log('Listing notes!')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Reading a note',
+    handler: function(){
+        console.log('Reading notes!')
+    }
+})
+
 console.log(yargs.argv)
-console.log(yargs.argv._)
-console.log(yargs.argv.title)
