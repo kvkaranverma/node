9c59c4554fa9ec56320f6307d432949ae7ed7774
kvkaranverma <42038789+kvkaranverma@users.noreply.github.com>
2020-06-12 22:22:55 +0530

    Refactoring to use Arrow functions

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -5,11 +5,13 @@ const getNotes = function(){
     return 'Your notes...';
 }
 
-const addNote = function (title, body) {
+const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter(function(note){
-        return note.title === title
-    })
+
+    // const duplicateNotes = notes.filter(function(note){
+    //     return note.title === title
+    // })
+    const duplicateNotes = notes.filter(note => note.title === title)
 
     if(duplicateNotes.length === 0) {
         notes.push({
@@ -24,11 +26,12 @@ const addNote = function (title, body) {
 
 }
 
-const removeNotes = function (title) {
+const removeNotes = (title) => {
     const notes = loadNotes()
-    const notesToKeep = notes.filter(function(note){
-        return note.title !== title
-    })
+    // const notesToKeep = notes.filter(function(note){
+    //     return note.title !== title
+    // })
+    const notesToKeep = notes.filter(note => note.title !== title)
     if(notes.length > notesToKeep.length){
         saveNotes(notesToKeep)
         console.log(chalk.bgGreen('Note deleted'))
@@ -38,12 +41,12 @@ const removeNotes = function (title) {
     }
 }
 
-const saveNotes = function (notes) {
+const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
 }
 
-const loadNotes = function () {
+const loadNotes = () => {
     try {
         const dataBuffer = fs.readFileSync('notes.json')
         const dataJSON = dataBuffer.toString();
