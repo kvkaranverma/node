c3cf9dcf568cacb9549c7e8b6461af80861512e0
kvkaranverma <42038789+kvkaranverma@users.noreply.github.com>
2020-06-12 18:57:52 +0530

    Arguments passing with yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -6,8 +6,21 @@ const yargs = require('yargs')
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function(){
-        console.log('Adding a new note!')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
     }
 })
 
@@ -15,6 +28,9 @@ yargs.command({
 yargs.command({
     command: 'remove',
     describe: 'Remove a note',
+    builder: {
+
+    },
     handler: function(){
         console.log('Remove the note')
     }
@@ -38,4 +54,5 @@ yargs.command({
     }
 })
 
-console.log(yargs.argv)
+yargs.parse()
+//console.log(yargs.argv)
