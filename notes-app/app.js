b568b70c812dc97afa148b294ecd9fbe1a79dc7c
kvkaranverma <42038789+kvkaranverma@users.noreply.github.com>
2020-06-12 17:53:42 +0530

    getting user input from console

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,3 +4,5 @@ const chalk = require('chalk')
 console.log(chalk.blue(notes()))
 console.log(chalk.bold.bgRed.green('success'))
 console.log(chalk.blue.inverse('danger'))
+
+console.log(process.argv[2])
