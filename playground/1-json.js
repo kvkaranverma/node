92a7bf90bf0197fd8ef729f0ba05fb74110e2b38
kvkaranverma <42038789+kvkaranverma@users.noreply.github.com>
2020-06-12 19:19:36 +0530

    Changing content of a file

--- playground/1-json.js
+++ playground/1-json.js
@@ -8,7 +8,16 @@ const fs = require('fs')
 // const bookJSON = JSON.stringify(Book)
 //fs.writeFileSync('1-json.json', bookJSON)
 
-const dataBuffer = fs.readFileSync('1-json.json')
-const dataJSON = dataBuffer.toString()
-const data = JSON.parse(dataJSON)
-console.log(data.title)
+// const dataBuffer = fs.readFileSync('1-json.json')
+// const dataJSON = dataBuffer.toString()
+// const data = JSON.parse(dataJSON)
+// console.log(data.title)
+
+const readFileBuffer = fs.readFileSync('1-json.json')
+const readFile = readFileBuffer.toString()
+const data = JSON.parse(readFile)
+data.name = 'Karan'
+data.age = '23'
+const jsonData = JSON.stringify(data)
+
+fs.writeFileSync('1-2-json.json', jsonData)
