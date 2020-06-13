0029a85f4398fe1b5dcac4dedef8484b5fbae783
kvkaranverma <42038789+kvkaranverma@users.noreply.github.com>
2020-06-13 19:34:01 +0530

    Dynamic views and Customizing views directory

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,9 +3,36 @@ const path = require('path')
 
 const app = express()
 
+//Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../views')
+
+// Setup handlebars engine and view location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
 
 app.use(express.static(publicDirectoryPath))
+
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Karan Verma'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Karan Verma'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send('<h2>Weather page up</h2>')
