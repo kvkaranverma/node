0c9ccd006ddc108c89a55c41ab7d966fda329ebb
kvkaranverma <42038789+kvkaranverma@users.noreply.github.com>
2020-06-14 10:42:39 +0530

    Advanced templating: Using partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const path = require('path')
+const hbs = require('hbs')
 
 const app = express()
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../views')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and view location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 app.use(express.static(publicDirectoryPath))
 
@@ -30,7 +33,8 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        title: 'Help'
+        title: 'Help',
+        name: 'Karan Verma'
     })
 })
 
