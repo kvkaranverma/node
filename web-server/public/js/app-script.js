61343c95c626288e4f820dc3c65d8525117293cd
kvkaranverma <42038789+kvkaranverma@users.noreply.github.com>
2020-06-14 13:28:34 +0530

    Creating search form and wiring up user interface

--- web-server/public/js/app-script.js
+++ web-server/public/js/app-script.js
@@ -1,13 +1,25 @@
 console.log('Client side js is loaded')
 
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        if(data.error) {
-            console.log(data.error)
-        }
-        else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#messageOne')
+const messageTwo = document.querySelector('#messageTwo')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if(data.error) {debugger
+                messageOne.textContent = data.error
+            }
+            else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
     })
 })
